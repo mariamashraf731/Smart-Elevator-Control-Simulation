710c63a8dda808b27192b31f3416e43265900e78
Mariam Ashraf El-Saqa <61399643+mariamashraf731@users.noreply.github.com>
2025-12-15 13:26:53 +0200

    Refactor elevator system code structure and comments

--- src/elevatorsystem.c
+++ src/elevatorsystem.c
@@ -1,118 +1,147 @@
 #include "C8051F020.h"
 
-// --- Global Variables ---
-int floor = 0;              // Current floor
-unsigned int up_request[5]   = {0, 0, 0, 0, 0};
-unsigned int floor_request[5]= {0, 0, 0, 0, 0};
-unsigned int down_request[5] = {0, 0, 0, 0, 0};
+/* -------------------------
+   Global variables
+------------------------- */
+int floor = 0;                        // Current elevator floor
 
-// --- Sensors & Actuators Mapping ---
-sbit redled     = P2^3;     // Door Closed / Moving
-sbit greenled   = P0^7;     // Door Open / Idle
-sbit OpenDoor   = P2^7;     // Manual Open Button
-sbit overload   = P1^7;     // Overload Indicator
+unsigned int up_request[5]   = {0,0,0,0,0};   // Up requests from floors
+unsigned int floor_request[5]= {0,0,0,0,0};   // Requests from inside elevator
+unsigned int down_request[5] = {0,0,0,0,0};   // Down requests from floors
 
-// Motor H-Bridge Control
-sbit motor1     = P2^0;     
-sbit motor2     = P2^1;     
-
-// IR Sensors for Passenger Counting
-sbit ir1        = P2^2;     
-sbit ir2        = P2^5;     
-
-// Input Buttons (Internal & External)
-sbit floor0     = P0^2;
-sbit floor1     = P0^3;
-sbit floor2     = P0^4;
-sbit floor3     = P0^5;
-sbit floor4     = P0^6;
-sbit floor0_down= P2^4;
-sbit floor2_down= P2^6;
-
-// State Variables
 int i = 0;
-int doorstate = 0;
-int enter = 0;
-int exit = 0;
-int persons = 0; 
 
-// --- Functions ---
+/* -------------------------
+   LEDs and indicators
+------------------------- */
+sbit redled   = P2^3;     // Red LED (door closed / moving)
+sbit greenled = P0^7;     // Green LED (door open)
 
-void timer0_init(void) {
-    CKCON &= 0xF0;          // Timer 0 uses SysClock/12 
-    TMOD = (TMOD & 0xF0) | 0x01; // Mode 1: 16-bit timer
-    TH0 = 0xFF;             // Reload value for ~240ms logic
-    TL0 = 0x63; 
-    ET0 = 1;                // Enable Timer0 Interrupt
+/* -------------------------
+   Door control
+------------------------- */
+int doorstate = 0;        // 0 = door open, 1 = door closed/moving
+sbit OpenDoor = P2^7;     // Manual open door button
+
+/* -------------------------
+   Inside elevator buttons
+------------------------- */
+sbit floor0 = P0^2;
+sbit floor1 = P0^3;
+sbit floor2 = P0^4;
+sbit floor3 = P0^5;
+sbit floor4 = P0^6;
+
+/* -------------------------
+   IR sensors for people count
+------------------------- */
+sbit ir1 = P2^2;          // IR sensor 1
+sbit ir2 = P2^5;          // IR sensor 2
+
+int enter = 0;            // Person entering flag
+int exit  = 0;            // Person exiting flag
+
+/* -------------------------
+   Overload detection
+------------------------- */
+sbit overload = P1^7;     // Overload indicator
+int persons = 0;          // Number of people inside elevator
+
+/* -------------------------
+   Motor control
+------------------------- */
+sbit motor1 = P2^0;       // Motor direction control
+sbit motor2 = P2^1;
+
+sbit floor0_down = P2^4;  // Down button at floor 0
+sbit floor2_down = P2^6;  // Down button at floor 2
+
+/* -------------------------
+   Timer0 initialization
+------------------------- */
+void timer0_init(void)
+{
+    CKCON &= 0xF0;                 // Timer0 uses SYSCLK/12
+    TMOD = (TMOD & 0xF0) | 0x01;   // Timer0 Mode 1 (16-bit)
+    TH0  = 0xFF;
+    TL0  = 0xBF;                   // ~240 ms overflow
+    ET0  = 1;                      // Enable Timer0 interrupt
 }
 
-// Blocking Delays (Simulation purpose)
-void motor_delay() {
-    int i, j;
-    for (i = 0; i < 1000; ++i)
-        for (j = 0; j < 100; ++j);
+/* -------------------------
+   Motor movement delays
+------------------------- */
+void motor_delay()
+{
+    int i,j;
+    for (i=0;i<1000;i++)
+        for(j=0;j<100;j++);
 }
 
-void motor_delay1() {
-    int i, j;
-    for (i = 0; i < 1000; ++i)
-        for (j = 0; j < 90; ++j);
+void motor_delay1()
+{
+    int i,j;
+    for (i=0;i<1000;i++)
+        for(j=0;j<90;j++);
 }
 
-void delay() {
-    int i, j;
-    for (i = 0; i < 500; ++i)
-        for (j = 0; j < 100; ++j);
+/* -------------------------
+   Small delay (debounce)
+------------------------- */
+void delay()
+{
+    int i,j;
+    for (i=0;i<500;i++)
+        for(j=0;j<100;j++);
 }
 
-// Passenger Counting Logic
-void check_sensors() {
-    // Logic to detect entry/exit sequence using 2 IR sensors
-    if ((ir1 == 0) && (ir2 == 1)) { 
-        if (exit == 0) enter = 1;
-        if (exit == 1) { persons--; exit = 0; delay(); }
-    }
-    if ((ir1 == 1) && (ir2 == 0)) { 
-        if (enter == 0) exit = 1;
-        if (enter == 1) { persons++; enter = 0; delay(); }
-    }
-}
+/* -------------------------
+   Door delay + people counting
+------------------------- */
+void door_delay()
+{
+    int i,j;
+    for (i=0;i<1000;i++)
+    for (j=0;j<1000;j++)
+    {
+        // Person entering
+        if ((ir1 == 0) && (ir2 == 1))
+        {
+            if(exit == 0) enter = 1;
+            if(exit == 1){
+                persons--;
+                exit = 0;
+                delay();
+            }
+        }
 
-void door_delay_monitoring() {
-    int i, j;
-    for (i = 0; i < 1000; ++i) {
-        for (j = 0; j < 1000; ++j) {
-            check_sensors(); // Keep monitoring while door is open
+        // Person exiting
+        if ((ir1 == 1) && (ir2 == 0))
+        {
+            if(enter == 0) exit = 1;
+            if(enter == 1){
+                persons++;
+                enter = 0;
+                delay();
+            }
         }
     }
 }
 
-void open_door() {
-    redled = 1;
-    greenled = 0;
-    door_delay_monitoring();
-    
-    // Safety Check: Overload
-    if (persons > 4) {
-        overload = 1;
-        open_door(); // Keep door open if overloaded
-    } else {
-        overload = 0;
-    }
-    
-    redled = 0;
-    greenled = 1;
-}
-
-void up() {
+/* -------------------------
+   Elevator movement
+------------------------- */
+void up()
+{
     motor1 = 1; motor2 = 0;
     motor_delay();
-    motor1 = 0; motor2 = 1; // Braking / Phase shift simulation
+    motor1 = 0; motor2 = 1;
     motor_delay1();
     motor2 = 0;
 }
 
-void down() {
+void down()
+{
     motor1 = 0; motor2 = 1;
     motor_delay();
     motor2 = 0; motor1 = 1;
@@ -120,75 +149,123 @@ void down() {
     motor1 = 0;
 }
 
-void requests(void) {
-    // Poll all buttons and update request arrays
-    if (!floor0) floor_request[0] = 1;
-    if (!floor1) floor_request[1] = 1;
-    if (!floor2) floor_request[2] = 1;
-    if (!floor3) floor_request[3] = 1;
-    if (!floor4) floor_request[4] = 1;
+/* -------------------------
+   Door operation
+------------------------- */
+void open_door()
+{
+    redled = 1;
+    greenled = 0;
 
-    // External Requests
-    if (!floor0_down) down_request[0] = 1;
-    if (!floor2_down) down_request[2] = 1;
-    
-    // Port 5 Polling for Up requests
-    if (!(P5 & 0x01)) up_request[1] = 1;
-    if (!(P5 & 0x02)) up_request[2] = 1;
-    if (!(P5 & 0x04)) up_request[3] = 1;
-    if (!(P5 & 0x08)) up_request[4] = 1;
+    door_delay();
 
-    if ((!OpenDoor && !doorstate)) {
-        open_door();
+    // Overload protection
+    if (persons > 4){
+        overload = 1;
+        open_door();   // keep door open
     }
+    if (persons < 5)
+        overload = 0;
+
+    redled = 0;
+    greenled = 1;
 }
 
-// ISR: Periodic polling for requests
-void timer0_ISR() interrupt 1 {
+/* -------------------------
+   Read all requests
+------------------------- */
+void requests(void)
+{
+    // Inside buttons
+    if(!floor0) floor_request[0] = 1;
+    if(!floor1) floor_request[1] = 1;
+    if(!floor2) floor_request[2] = 1;
+    if(!floor3) floor_request[3] = 1;
+    if(!floor4) floor_request[4] = 1;
+
+    // Down buttons
+    if(!floor0_down) down_request[0] = 1;
+    if(!floor2_down) down_request[2] = 1;
+
+    // Up buttons on P5
+    if(!(P5 & 0x01)) up_request[1] = 1;
+    if(!(P5 & 0x02)) up_request[2] = 1;
+    if(!(P5 & 0x04)) up_request[3] = 1;
+    if(!(P5 & 0x08)) up_request[4] = 1;
+
+    // Manual open door
+    if(!OpenDoor && !doorstate)
+        open_door();
+}
+
+/* -------------------------
+   Timer0 ISR
+------------------------- */
+void timer0_ISR() interrupt 1
+{
     TF0 = 0;
-    requests();
+    requests();     // Periodically scan requests
 }
 
-void seg_display(int fl) {
-    if (fl == 0) P1 = 0x3F; // '0'
-    else if (fl == 1) P1 = 0x06; // '1'
-    else if (fl == 2) P1 = 0x5B; // '2'
-    else if (fl == 3) P1 = 0x4F; // '3'
-    else if (fl == 4) P1 = 0x66; // '4'
+/* -------------------------
+   7-segment display
+------------------------- */
+void seg_dispaly(int floor)
+{
+    if(floor==0) P1 = 0x3F;
+    else if(floor==1) P1 = 0x06;
+    else if(floor==2) P1 = 0x5B;
+    else if(floor==3) P1 = 0x4F;
+    else if(floor==4) P1 = 0x66;
 }
 
-void main(void) {
-    // System Init
-    WDTCN = 0xDE; WDTCN = 0xAD; // Disable Watchdog
-    OSCICN = 0x14;              // 2MHz internal clock
-    XBR0 = 0x00; XBR1 = 0x00; XBR2 = 0x40; // Crossbar enabled
-    
-    // Port Configuration
+/* -------------------------
+   MAIN
+------------------------- */
+void main(void)
+{
+    // Disable watchdog
+    WDTCN = 0xDE;
+    WDTCN = 0xAD;
+
+    OSCICN = 0x14;       // 2 MHz internal clock
+
+    // Crossbar config
+    XBR0 = 0x00;
+    XBR1 = 0x00;
+    XBR2 = 0x40;
+
+    // Port direction
     P0MDOUT |= 0x80;
     P1MDOUT |= 0x7F;
     P2MDOUT |= 0x00;
 
     timer0_init();
-    EA = 1; TR0 = 1; // Enable Global Interrupts & Timer0
+    EA  = 1;
+    TR0 = 1;
 
     overload = 0;
-    redled = 0; greenled = 1;
-    requests();
+    redled   = 0;
+    greenled = 1;
 
-    while (1) {
-        seg_display(floor);
-        
-        // UP Direction Sweep (SCAN Algorithm)
-        for (i = 0; i < 5; i++) {
-            if ((floor_request[i] || up_request[i] || down_request[i]) && i > floor) {
-                while (floor != i) {
+    while(1)
+    {
+        seg_dispaly(floor);
+
+        // Move UP
+        for(i=0;i<5;i++)
+        {
+            if((floor_request[i] || up_request[i] || down_request[i]) && i > floor)
+            {
+                while(floor != i)
+                {
                     doorstate = 1;
                     up();
                     floor++;
-                    seg_display(floor);
-                    
-                    // Check for intermediate stops
-                    if (floor_request[floor] || up_request[floor] || down_request[floor]) {
+                    seg_dispaly(floor);
+
+                    if(floor_request[floor] || up_request[floor] || down_request[floor])
+                    {
                         floor_request[floor] = 0;
                         up_request[floor] = 0;
                         down_request[floor] = 0;
@@ -199,16 +276,20 @@ void main(void) {
             }
         }
 
-        // DOWN Direction Sweep
-        for (i = 4; i >= 0; i--) {
-            if ((floor_request[i] || down_request[i] || up_request[i]) && i < floor) {
-                while (floor != i) {
+        // Move DOWN
+        for(i=4;i>=0;i--)
+        {
+            if((floor_request[i] || down_request[i] || up_request[i]) && i < floor)
+            {
+                while(floor != i)
+                {
                     doorstate = 1;
                     down();
                     floor--;
-                    seg_display(floor);
-                    
-                    if (floor_request[floor] || down_request[floor] || up_request[floor]) {
+                    seg_dispaly(floor);
+
+                    if(floor_request[floor] || down_request[floor] || up_request[floor])
+                    {
                         floor_request[floor] = 0;
                         down_request[floor] = 0;
                         up_request[floor] = 0;
